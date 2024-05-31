a8ed5b611369f0f03b324fbb4831b427934979c5
Harshuuuu2010 <harshdarji669@gmail.com>
2024-05-31 13:48:34 +0530

    second commit

--- ecomern-frontend-main/src/axios.js
+++ ecomern-frontend-main/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080",
+  baseURL: "https://shopper-1-yzv9.onrender.com",
 });
 
 export default instance;
